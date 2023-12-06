88a14d84ad92b25fc400d9a38f5e17fbd706d47c
Sircule14 <victor.jromar@gmail.com>
2023-12-06 14:07:13 -0700

    parcial changes

---
+++ js/aurum.js
@@ -0,0 +1,23 @@
+const images = ["image1.jpg", "image2.jpg"];
+let currentIndex = 0;
+
+const currentImgElement = document.getElementById("current-img");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+
+function showImage(index) {
+    currentImgElement.src = images[index];
+}
+
+prevBtn.addEventListener("click", function() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+});
+
+nextBtn.addEventListener("click", function() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+});
+
+// Show the initial image
+showImage(currentIndex);
